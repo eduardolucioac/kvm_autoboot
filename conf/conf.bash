cec62d795695a064964f42656583202005649277
edulucio <eduardo.lucio@lightbase.com.br>
2020-02-27 10:37:56 -0300

    Initial release. By Questor

--- conf/conf.bash
+++ conf/conf.bash
@@ -3,10 +3,11 @@
 # > -----------------------------------------
 # * KVM_AUTOBOOT SERVICE CONFIGURATION
 
-# NOTE: List with the VMs's names in the desired boot start order and the interval
-# to boot next (in seconds). The first VM in the list boots with the service and
-# the others after the defined interval immediately before its name. If the interval
-# is set to "-1", then the default interval BOOT_DEF_INTERV will be used. By Questor
+# NOTE: List with the VMs's names - actually a bash "array" - in the desired boot
+# order and the interval (in seconds) to boot each VM in the sequence. The first VM
+# in the list boots with the service and the others after the defined interval immediately
+# before their name. If the interval is set to "-1", then the default interval BOOT_DEF_INTERV
+# will be used. By Questor
 BOOT_ORDER=("VM_NAME0" CUSTOM_INTERV1 "VM_NAME1" CUSTOM_INTERV2 "VM_NAME2")
 
 # NOTE: Default interval between the start of a VM and another one in seconds.
